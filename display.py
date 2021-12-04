3afb41ecc0a30c421fbb607be6513da7a0829e2b
Pritam Dhara <65482473+hDmtP@users.noreply.github.com>
2021-12-04 17:56:54 +0530

    Update display.py

--- display.py
+++ display.py
@@ -2,7 +2,7 @@ from reddit_dose import *
 
 
 def choose():
-    choice = int(input("0 -> sub_comment_stream | 1 -> sub_submissions_hot | 2 -> sub_submission_stream | 3 -> redditor_stream_comnts | 4 -> redditor_stream_submissions: "))
+    choice = int(input("\n\n\n0 -> sub_comment_stream | 1 -> sub_submissions_hot | 2 -> sub_submission_stream | 3 -> redditor_stream_comnts | 4 -> redditor_stream_submissions | 5 -> sub_submissions_top | 6 -> search | 7 -> close(): "))
     if(choice == 0):
         try:
             sub_comment_stream()
@@ -26,6 +26,16 @@ def choose():
     elif(choice == 4):
         try:
             redditor_stream_submissions()
+        except KeyboardInterrupt:
+            print("\n\tAdios!\n\t")
+    elif(choice == 5):
+        try:
+            sub_submissions_top()
+        except KeyboardInterrupt:
+            print("\n\tAdios!\n\t")
+    elif(choice == 6):
+        try:
+            search_sub()
         except KeyboardInterrupt:
             print("\n\tAdios!\n\t")
 
