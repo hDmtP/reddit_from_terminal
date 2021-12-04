e34bf253c8e813bac58e2af4f08ade2097eced14
Pritam Dhara <65482473+hDmtP@users.noreply.github.com>
2021-12-04 17:56:19 +0530

    Update reddit_dose.py

--- reddit_dose.py
+++ reddit_dose.py
@@ -19,7 +19,9 @@ def authenticate():
 reddit = authenticate()
 
 if reddit.read_only:
-    print("\tAll set, ready to explore Reddit!\t")
+    print("\n\n\t\t=======================================\t\t\t")
+    print("\t\t|| All set, ready to explore Reddit! ||\t\t\t")
+    print("\t\t=======================================\t\t\t")
 
 
 def sub_comment_stream():
@@ -35,7 +37,8 @@ def sub_submission_stream():
     subreddit = reddit.subreddit(sub)
     for submission in subreddit.stream.submissions(skip_existing=True, pause_after=5):
         print("\n\n\n")
-        print(f"user:{submission.author}\nid:{submission}\ntitle:{submission.title}\n\n")
+        print(
+            f"user:{submission.author}\nid:{submission}\ntitle:{submission.title}\n\n")
         print(f"Link:https://www.reddit.com/r/{sub}/comments/{submission}/")
 
 
@@ -45,7 +48,8 @@ def sub_submissions_hot():
     limit = int(input("Limit: "))
     for submission in subreddit.hot(limit=limit):
         print("\n\n\n")
-        print(f"user:{submission.author}\nid:{submission}\ntitle:{submission.title}\n\n")
+        print(
+            f"user:{submission.author}\nid:{submission}\ntitle:{submission.title}\n\n")
         print(f"Link:https://www.reddit.com/r/{sub}/comments/{submission}/")
 
 
@@ -61,3 +65,43 @@ def redditor_stream_submissions():
     for submission in reddit.redditor(user).stream.submissions():
         print("\n\n\n")
         print(f"id:{submission}\ntitle:{submission.title}")
+
+
+def sub_submissions_top():
+    sub = str(input("Enter subreddit name: "))
+    subreddit = reddit.subreddit(sub)
+    limit = int(input("Limit: "))
+    for submission in subreddit.top(limit=limit):
+        print("\n\n\n")
+        print(
+        f"user:{submission.author}\nid:{submission}\ntitle:{submission.title}\n\n")
+        print(f"Link:https://www.reddit.com/r/{sub}/comments/{submission}/")
+
+
+def search_sub():
+    keyword = str(input("Enter keyword for search: "))
+    sub = str(input("Enter subreddit name: "))
+    i = 0
+    import re
+
+    def findWholeWord(w):
+        return re.compile(r'\b({0})\b'.format(w), flags=re.IGNORECASE).search
+
+    subreddit = reddit.subreddit(sub)
+    for submission in subreddit.hot():
+        if findWholeWord(keyword)(str(submission.title)) != None:
+            i = i+1
+            print(f"Link:https://www.reddit.com/r/{sub}/comments/{submission}/\n\n")
+            print(f"total items found till....: {i}\n\n")
+
+    for submission in subreddit.new():
+        if findWholeWord(keyword)(str(submission.title)) != None:
+            i = i+1
+            print(f"Link:https://www.reddit.com/r/{sub}/comments/{submission}/\n\n")
+            print(f"total items found till....: {i}\n\n")
+
+    for submission in subreddit.top():
+        if findWholeWord(keyword)(str(submission.title)) != None:
+            i = i+1
+            print(f"Link:https://www.reddit.com/r/{sub}/comments/{submission}/\n\n")
+            print(f"total items found till....: {i}\n\n")
