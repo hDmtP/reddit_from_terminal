c9f1aa505ea5418e45b6ec404869cdb28f43af24
hDmtP <pritamdhara914@gmail.com>
2021-12-29 21:48:55 +0530

    made it a script

--- reddit_dose.py
+++ reddit_dose.py
@@ -1,3 +1,4 @@
+#!/usr/bin/python3
 import praw
 import fontstyle
 import os
@@ -137,3 +138,55 @@ def search_user_comment():
             i = i+1
             print(fontstyle.apply(f"\n\nuser:{comment.author}\nid:{comment}\ncomment:{comment.body}\n\n", "bold/darkcyan"))
             print(fontstyle.apply(f"total items found till....: {i}\n\n", "Italic/yellow"))
+
+def choose():
+    choice = int(input(fontstyle.apply("\n\n\n 0 -> sub_comment_stream \n\n 1 -> sub_submissions_hot \n\n 2 -> sub_submission_stream \n\n 3 -> redditor_stream_comnts \n\n 4 -> redditor_stream_submissions \n\n 5 -> sub_submissions_top \n\n 6 -> search subreddit \n\n 7 -> search comments \n\n 8 -> search an user's comment \n\n 9 -> exit(): ", "bold/Italic/purple")))
+    
+    if(choice == 0):
+        try:
+            sub_comment_stream()
+        except KeyboardInterrupt:
+            print(fontstyle.apply("\n\tAdios!\n\t", "bold/Italic/red"))
+    elif(choice == 1):
+        try:
+            sub_submissions_hot()
+        except KeyboardInterrupt:
+            print(fontstyle.apply("\n\tAdios!\n\t", "bold/Italic/red"))
+    elif(choice == 2):
+        try:
+            sub_submission_stream()
+        except KeyboardInterrupt:
+            print(fontstyle.apply("\n\tAdios!\n\t", "bold/Italic/red"))
+    elif(choice == 3):
+        try:
+            redditor_stream_comnts()
+        except KeyboardInterrupt:
+            print(fontstyle.apply("\n\tAdios!\n\t", "bold/Italic/red"))
+    elif(choice == 4):
+        try:
+            redditor_stream_submissions()
+        except KeyboardInterrupt:
+            print(fontstyle.apply("\n\tAdios!\n\t", "bold/Italic/red"))
+    elif(choice == 5):
+        try:
+            sub_submissions_top()
+        except KeyboardInterrupt:
+            print(fontstyle.apply("\n\tAdios!\n\t", "bold/Italic/red"))
+    elif(choice == 6):
+        try:
+            search_sub()
+        except KeyboardInterrupt:
+            print(fontstyle.apply("\n\tAdios!\n\t", "bold/Italic/red"))
+    elif(choice == 7):
+        try:
+            search_comment()
+        except KeyboardInterrupt:
+            print(fontstyle.apply("\n\tAdios!\n\t", "bold/Italic/red"))
+    elif(choice == 8):
+        try:
+            search_user_comment()
+        except KeyboardInterrupt:
+            print(fontstyle.apply("\n\tAdios!\n\t", "bold/Italic/red"))
+
+if __name__ == "__main__":
+	choose()
